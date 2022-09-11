aca771b3d13ccb01d30c3ca10d8e4c794683146e
daringthiago <thiago_bm2009@hotmail.com>
2022-09-10 23:51:59 -0300

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "thiago", password: "123", only: :destroy
+
+  def create
+    @article = Article.find(params[:article_id])
+    # ...
+  end
+
+  # snippet for brevity
+
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
+
+end
